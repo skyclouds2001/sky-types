691922f89ec65e62b93074c84d88b80468607a85
c50034865 <chensiyuan49@huawei.com>
2023-07-19 09:36:06 +0800

    feat: add export support

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export type { Awaitable, Promisify } from './any'
 
 export type { Primitive, Builtin } from './core'
 
-export type { Function } from './function'
+export type { Constructor, Function } from './function'
 
 export type { Partial, Readonly, Record, Required, Writable } from './list'
 
