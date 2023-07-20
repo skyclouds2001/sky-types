f9781da1c026be221fe34e04055f36db45dedbbd
c50034865 <chensiyuan49@huawei.com>
2023-07-20 16:31:00 +0800

    feat: add more tool type

--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,11 @@
 export type Function = (...args: any[]) => any
 
 export type Constructor = abstract new (...args: any[]) => any
+
+export type Parameters<T extends (...args: any[]) => any> = T extends (...args: infer P) => any ? P : never
+
+export type ReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer R ? R : any
+
+export type ConstructorParameters<T extends abstract new (...args: any[]) => any> = T extends abstract new (...args: infer P) => any ? P : never
+
+export type InstanceType<T extends abstract new (...args: any[]) => any> = T extends abstract new (...args: any[]) => infer R ? R : any
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export type { Awaitable, Exclude, Extract, NonNullable, Promisify } from './any'
 
 export type { Primitive, Builtin } from './core'
 
-export type { Constructor, Function } from './function'
+export type { Constructor, ConstructorParameters, Function, InstanceType, Parameters, ReturnType } from './function'
 
 export type { Omit, Partial, Pick, Readonly, Record, Required, Writable } from './list'
 
